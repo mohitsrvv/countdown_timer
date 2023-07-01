0ac222d1b4527a21612239668f631f52086bc7b2
mohit <mohitt0704@gmaiil.com>
2023-07-01 19:30:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minsE1 = document.getElementById('mins');
+const secondsE1 = document.getElementById('seconds');
+
+const newYears = '1 Jan 2024';
+
+function countdown() {
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minsE1.innerHTML = mins;
+    secondsE1.innerHTML = seconds;
+}
+
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
+
